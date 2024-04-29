f890677c0a1477deadf2550d9c97a86c28fbabdf
frank-Regal <fregal@utexas.edu>
2024-04-28 22:23:30 -0500

    working dataset builder

--- scripts/config_params.py
+++ scripts/config_params.py
@@ -8,43 +8,53 @@ from std_msgs.msg import Empty
 class ConfigParams():
     
     '''
-    initialize the ros params
+    Initialize the ros params
     '''
     def __init__(self):
         # get params
-        start_topic_name = rospy.get_param('/start_topic_name', 'n/a')
         stop_topic_name = rospy.get_param('/stop_topic_name', 'n/a')
         
-        # Setup subscribers
-        rospy.Subscriber(stop_topic_name, Empty, self.reset_cb, queue_size=10)
+        # setup stop subscribers
+        self.stop_sub = rospy.Subscriber(stop_topic_name, Empty, self.reset_cb, queue_size=10)
 
+        # init first file names
         self.classname_id = self.set_class_id()
         self.timestamp = self.set_timestamp()
         self.set_filename_per_device()
 
     '''
-    set class id
+    Set class id
     '''
     def set_class_id(self):
-        # get class id
+        # get current class name
         classname = rospy.get_param('/classname', 'na')
+
+        # dynamically set the class param name
         classname_id_param = "/class/" + classname + "/id"
+
+        # get the current class id
         classname_id = rospy.get_param(classname_id_param, 'na')
+
+        # set the parameter for all nodes to access
         rospy.set_param('/classname_id', classname_id)
+
         return classname_id
     
     '''
-    set fileneame per device
+    Set filename for saving files on each device
     '''
     def set_filename_per_device(self):
+        # get list of devices
         devices = rospy.get_param('/device/','n/a')
+
+        # set the filenames for each device
         for i, name in enumerate(devices):
             device_id = devices[name]['id']
-            filename = 'S' + self.timestamp + '-D' + device_id + '-C' + self.classname_id + '.avi'
+            filename = 'S' + self.timestamp + '-D' + device_id + '-C' + self.classname_id
             rospy.set_param('/'  + name + '/filename', filename)
 
     '''
-    set timestamp parameter
+    Set timestamp parameter
     '''
     def set_timestamp(self):
         # Get the current datetime
@@ -58,16 +68,27 @@ class ConfigParams():
 
         return formatted_time
     
-    def reset_cb(self):
-        self.set_timestamp()
+    '''
+    Callback when "stop" messsage is heard
+    '''
+    def reset_cb(self, msg):
         
-
+        # reset the filenames
+        self.timestamp = self.set_timestamp()
+        self.set_filename_per_device()
+        
+        # log
+        rospy.logwarn("Writing Finished. Filenames updated, ready for next set of data.")
+        
 def main():
     # Initialize the ROS node
     rospy.init_node('config')
     
     # setup class object
     cfg_params = ConfigParams()
+
+    # spin
+    rospy.spin()
 
 
 if __name__ == '__main__':
