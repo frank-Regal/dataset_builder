371daa536b3c871245a690b2f9420ccbd833cb18
frankRegal <fregal@utexas.edu>
2025-02-19 18:25:29 -0600

    slight modifications

--- scripts/start_stop.py
+++ scripts/start_stop.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Empty
+from std_msgs.msg import Empty, String
 import sys
 import tty
 import termios
@@ -23,12 +23,14 @@ def main():
 
     # Create publishers
     start_pub = rospy.Publisher('/hri_cacti/dataset_capture/start', Empty, queue_size=1)
+    start_pub_class = rospy.Publisher('/hri_cacti/dataset_capture/start/class', String, queue_size=1)
     stop_pub = rospy.Publisher('/hri_cacti/dataset_capture/stop', Empty, queue_size=1)
 
     print("Press 's' to send start message")
     print("Press 'p' to send stop message")
     print("Press 'q' to quit")
-    print(f'\n')
+    print("Classes:")
+    print("'1' : 'advance'")
 
     pair_count = 0
     waiting_for_stop = False
@@ -44,25 +46,35 @@ def main():
             if char is not None:
                 if char == 's' and not waiting_for_stop:
                     start_pub.publish(Empty())
-                    sys.stdout.write('\r')  # Return to start of line
+                    sys.stdout.write('\r')
                     sys.stdout.flush()
                     print(f"<= Start message sent. (Waiting for stop) =>", end="\n")
                     print(f'', end="\r")
                     waiting_for_stop = True
                 elif char == 'p' and waiting_for_stop:
                     stop_pub.publish(Empty())
-                    sys.stdout.write('\r')  # Return to start of line
+                    sys.stdout.write('\r')
                     sys.stdout.flush()
                     pair_count += 1
                     print(f"Stop message sent! (Collection #{pair_count} Complete)", end="\n")
                     print(f'', end="\r")
                     waiting_for_stop = False
                 elif char == 'q':
-                    sys.stdout.write('\r')  # Return to start of line
+                    sys.stdout.write('\r')
                     sys.stdout.flush()
                     print(f"Exiting... Recorded {pair_count} start-stop pairs", end="\r")
                     print(f'\n')
                     break
+
+                elif char == '1' and not waiting_for_stop:
+                    msg = String()
+                    msg.data = 'advance'
+                    start_pub_class.publish(msg)
+                    sys.stdout.write('\r')
+                    sys.stdout.flush()
+                    print(f"<= Start msg sent for 'advance' (Waiting for stop) =>", end="\n")
+                    print(f'', end="\r")
+                    waiting_for_stop = True
 
             rospy.sleep(0.1)  # Small sleep to prevent CPU hogging
 
